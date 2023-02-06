ba1dfaad6e8b12e4da9073dee86d2b27966c1ca2
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-02-07 01:53:55 +0900

    HTMLを取り扱う: 💪 演習: 複数の属性値列を取り扱う

--- src/html.rs
+++ src/html.rs
@@ -1,7 +1,7 @@
 use crate::dom::{AttrMap, Element, Node};
 use combine::error::ParseError;
 use combine::parser::char::{char, letter, newline, space};
-use combine::{between, many, many1, satisfy, Parser, Stream};
+use combine::{between, many, many1, satisfy, sep_by, Parser, Stream};
 
 fn attribute<Input>() -> impl Parser<Input, Output = (String, String)>
 where
@@ -20,6 +20,22 @@ where
         ),
     )
         .map(|v| (v.0, v.4)) // はじめに読んだ属性の名前と、最後に読んだ引用符の中の文字列を結果として返す
+}
+
+// `attributes` consumes `name1="value1" name2="value2" ... name="value"`
+fn attributes<Input>() -> impl Parser<Input, Output = AttrMap>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    sep_by::<Vec<(String, String)>, _, _, _>(
+        attribute(),
+        many::<String, _, _>(space().or(newline())),
+    )
+    .map(|attrs: Vec<(String, String)>| {
+        let m: AttrMap = attrs.into_iter().collect();
+        m
+    })
 }
 
 #[cfg(test)]
@@ -41,5 +57,18 @@ mod tests {
             attribute().parse("test = \"foobar\""),
             Ok((("test".to_string(), "foobar".to_string()), ""))
         );
+    }
+
+    #[test]
+    fn test_parse_attributes() {
+        let mut expected_map = AttrMap::new();
+        expected_map.insert("test".to_string(), "foobar".to_string());
+        expected_map.insert("abc".to_string(), "def".to_string());
+        assert_eq!(
+            attributes().parse("test=\"foobar\" abc=\"def\""),
+            Ok((expected_map, ""))
+        );
+
+        assert_eq!(attributes().parse(""), Ok((AttrMap::new(), "")))
     }
 }
